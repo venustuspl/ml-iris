dc452f62b89c97b52b66426b457392e7cb8b681c
tom <venustus.pl@gmail.com>
2024-05-08 17:09:35 +0200

    predict miles per galon

--- 3_12 mpg.py
+++ 3_12 mpg.py
@@ -25,7 +25,9 @@ lr.score(X.to_numpy(),y)
 # Korzystanie z modelu
 my_car1 = [4, 160, 190, 12, 90, 1]
 my_car2 = [4, 200, 260, 15, 83, 1]
-cars = [my_car1, my_car2]
+my_car3 = [5, 188, 160, 11, 91, 1]
+my_car4 = [5, 288, 277, 14, 84, 1]
+cars = [my_car1, my_car2, my_car3, my_car4]
 
 mpg_predict = lr.predict(cars)
 print(mpg_predict)
